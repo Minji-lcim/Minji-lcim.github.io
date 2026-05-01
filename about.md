e9b794845838cf92fc9899ed3c7c91e6c59c0de1
Minji-lcim <snamu5284@snu.ac.kr>
2026-05-01 11:12:12 +0000

    과제 수행

--- about.md
+++ about.md
@@ -4,10 +4,14 @@ title: "소개"
 ---
 
 # 자기소개
-안녕하세요! 저는 서울대학교 인문대학에 재학중인 학생입니다. 
+안녕하세요! 저는 <a href="https://humanities.snu.ac.kr/">*서울대학교 인문대학*</a>에 재학중인 학생입니다. 
 
 ## 이 사이트에 대하여
-이 사이트는 **인문데이터과학개론**수업에서 HTML, CSS, JavaScript, Jekyll을 배우며 만들었습니다. 
+이 사이트는 **인문데이터과학개론**수업에서 만들었습니다. 강의에서 배우는 내용은 아래와 같습니다. 
+- HTML 
+- CSS
+- JavaScript
+- Jekyll
 
 
 <script src="/assets/js/hello.js"></script>
--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,12 +1,9 @@
 // assets/js/hello.js
-const author = "이상";
-const title = "날개";
-const year = 1936;
+const title = "김민지의 웹사이트";
 const today = 2026;
-const yearsPassed = today - year;
 
 console.log(
-    `안녕하세요! ${author}의 「${title}」 (${year}) ` +
-    `페이지에 오신 것을 환영합니다.`
+    '안녕하세요! ${title}' +
+    '페이지에 오신 것을 환영합니다.'
  );
-console.log(`이 작품은 발표된 지 ${yearsPassed}년이 되었습니다.`);
+console.log('이 사이트는 ${today}년에 만들었습니다.');
